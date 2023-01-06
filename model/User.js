fafdadeda1bfe8fbfa9992da45bf1d164267fa24
song-na-min <minnasong0707@gmail.com>
2023-01-06 21:33:22 +0900

    #28 conf: User.js

--- model/User.js
+++ model/User.js
@@ -19,30 +19,42 @@ const userSchema = new Schema({
   },
 
   //취향 추천용 태그(최대 5개) tasteTags: [ {Object id(ref Tag)}  ]
-  tasteTags: {
-    type: Array,
-  },
+  tasteTags: [
+    {
+      type: mongoose.Schema.Type.ObjectId,
+      ref: "Tag",
+    },
+  ],
   //tagedWebtoons(내가 태그 설정한 웹툰 목록)
   //웹툰-내가 설정한 태그 같이 저장 필요[ {webtoon_id :[tag_id…]} ]
-  tagedWebtoons: {
-    type: Array,
-  },
+  tagedWebtoons: [
+    {
+      webtoon_taged: { type: mongoose.Schema.Type.ObjectId, ref: "Webtoon" },
+      tags: [{ type: mongoose.Schema.Type.ObjectId, ref: "Tag" }],
+    },
+  ],
 
   // likedWebtoons(관심 웹툰 목록): [
   //   {Object id (ref Webtoon)}  }
-  likedWebtoons: {
-    type: Array,
-  },
+  likedWebtoons: [
+    {
+      type: mongoose.Schema.Type.ObjectId,
+      ref: "Webtoon",
+    },
+  ],
 
   // likedComments(좋아요 누른 댓 목록): [
   //   {Object id (ref Comments)}}
-  likedComments: {
-    type: Array,
-  },
+  likedComments: [
+    {
+      type: Array,
+    },
+  ],
 
   //{Object Id (ref Comments) }]
   myComments: {
-    type: Array,
+    type: mongoose.Schema.Type.ObjectId,
+    ref: "Comment",
   },
 
   roles: {
