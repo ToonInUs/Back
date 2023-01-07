c2bf3eff2036fc00dee35f18cd02b156e0382d54
song-na-min <minnasong0707@gmail.com>
2023-01-07 23:01:38 +0900

    #34 add models

--- model/User.js
+++ model/User.js
@@ -21,24 +21,25 @@ const userSchema = new Schema({
   //취향 추천용 태그(최대 5개) tasteTags: [ {Object id(ref Tag)}  ]
   tasteTags: [
     {
-      type: mongoose.Schema.Type.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Tag",
     },
   ],
   //tagedWebtoons(내가 태그 설정한 웹툰 목록)
   //웹툰-내가 설정한 태그 같이 저장 필요[ {webtoon_id :[tag_id…]} ]
-  tagedWebtoons: [
-    {
-      webtoon_taged: { type: mongoose.Schema.Type.ObjectId, ref: "Webtoon" },
-      tags: [{ type: mongoose.Schema.Type.ObjectId, ref: "Tag" }],
-    },
-  ],
+  //일단 주석처리해서 수정
+  // tagedWebtoons: [
+  //   {
+  //     webtoon_taged: { type:Schema.Types.ObjectId, ref: "Webtoon" },
+  //     tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  //   },
+  // ],
 
   // likedWebtoons(관심 웹툰 목록): [
   //   {Object id (ref Webtoon)}  }
   likedWebtoons: [
     {
-      type: mongoose.Schema.Type.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Webtoon",
     },
   ],
@@ -53,7 +54,7 @@ const userSchema = new Schema({
   //merge test
   //{Object Id (ref Comments) }]
   myComments: {
-    type: mongoose.Schema.Type.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: "Comment",
   },
 
